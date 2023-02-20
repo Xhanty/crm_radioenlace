7a3a69e5e4c01bf5bba0c8932be6eb9b06d62817
Santiago Henao <santiagosmithdelgadohenao@gmail.com>
2023-02-20 17:27:34 -0500

    Avance 20/02/2023

--- resources/views/admin/contabilidad/configuracion/formas_pago.blade.php
+++ resources/views/admin/contabilidad/configuracion/formas_pago.blade.php
@@ -15,12 +15,29 @@
                 <div class="row row-sm">
                     <div class="col-lg mg-t-10 mg-lg-t-0">
                         <label for="">Forma de pago</label>
-                        <input class="form-control" id="forma_pago_organizacion" placeholder="Forma de pago" type="text">
+                        <input class="form-control" id="forma_pago_organizacion" placeholder="Forma de pago"
+                            type="text">
                     </div>
                 </div>
                 <br>
                 <div class="text-center">
-                    <button class="btn ripple btn-primary" id="btnFormaPago" type="button">Agregar</button>
+                    <button class="btn ripple btn-primary" id="btnFormaPago" type="button">Agregar Nuevo</button>
+                </div>
+                <br>
+                <div class="table-responsive">
+                    <table id="tbl_formas_pago" class="table border-top-0 table-bordered text-nowrap border-bottom basic-datatable-t">
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Creador</th>
+                                <th>Status</th>
+                                <th>Fecha</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
