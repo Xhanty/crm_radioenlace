8d8c5b884fa841f087c59cd4eaf9f66c70a2b7ed
Santiago Henao <santiagosmithdelgadohenao@gmail.com>
2023-10-17 15:29:55 -0500

    Avance

--- app/Exports/FacturaVentaSheet.php
+++ app/Exports/FacturaVentaSheet.php
@@ -3,12 +3,15 @@
 namespace App\Exports;
 
 use Carbon\Carbon;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 use Maatwebsite\Excel\Concerns\WithTitle;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
-class FacturaVentaSheet implements FromCollection, WithHeadings, WithTitle
+class FacturaVentaSheet implements FromCollection, WithHeadings, WithTitle, WithColumnFormatting
 {
     use Exportable;
 
@@ -22,7 +25,13 @@ class FacturaVentaSheet implements FromCollection, WithHeadings, WithTitle
     public function collection()
     {
         // Transformar los datos para mostrar las columnas
-        return $this->transformData();
+        $transformedData = $this->transformData();
+
+        // Agregar una fila de totales
+        $totals = $this->calculateTotals($transformedData);
+        $transformedData[] = $totals;
+
+        return collect($transformedData);
     }
 
     public function headings(): array
@@ -32,9 +41,16 @@ class FacturaVentaSheet implements FromCollection, WithHeadings, WithTitle
             'Factura',
             'Identificación',
             'Razón Social',
+            'Total Bruto',
+            'Descuentos',
+            'Subtotal',
+            "Iva (%)",
             "Iva",
+            "Rte Fte (%)",
             "Rte Fte",
+            "Rte Iva (%)",
             "Rte Iva",
+            "Rte Ica (%)",
             "Rte Ica",
             'Valor',
             'Estado',
@@ -56,6 +72,8 @@ class FacturaVentaSheet implements FromCollection, WithHeadings, WithTitle
                 $status = "Pendiente";
             }
 
+            $impuestos_1 = json_decode($item->impuestos_1);
+
             $subtotal = str_replace(',', '.', str_replace('.', '', $item->subtotal));
             $subtotal = (float) $subtotal;
 
@@ -63,35 +81,100 @@ class FacturaVentaSheet implements FromCollection, WithHeadings, WithTitle
             $html_rte_fte = "";
             $html_rte_iva = "";
             $html_rte_ica = "";
+            $valor_iva = "";
 
-            if($item->valor_retefuente) {
-                $html_rte_fte = "(" . $item->valor_retefuente . "%): ";
-                $html_rte_fte .= $item->valor_retefuente * $subtotal / 100;
+            if ($impuestos_1 && count($impuestos_1) > 0) {
+                for ($i = 0; $i < count($impuestos_1); $i++) {
+                    $valor_iva = $impuestos_1[$i][0];
+                    $html_iva = number_format($impuestos_1[$i][1], 2, ',', '.');
+                }
             }
 
-            if($item->valor_reteiva) {
-                $html_rte_iva = "(" . $item->valor_reteiva . "%): ";
-                $html_rte_iva .= $item->valor_reteiva * $subtotal / 100;
+            if ($item->valor_retefuente) {
+                $html_rte_fte .= number_format($item->valor_retefuente * $subtotal / 100, 2, ',', '.');
             }
 
-            if($item->valor_reteica) {
-                $html_rte_ica = "(" . $item->valor_reteica . "%): ";
-                $html_rte_ica .= $item->valor_reteica * $subtotal / 1000;
+            if ($item->valor_reteiva) {
+                $html_rte_iva .= number_format($item->valor_reteiva * $subtotal / 100, 2, ',', '.');
             }
-            
+
+            if ($item->valor_reteica) {
+                $html_rte_ica .= number_format($item->valor_reteica * $subtotal / 1000, 2, ',', '.');
+            }
+
             return [
                 "FE-" . $item->numero,
                 $item->nit . "-" . $item->codigo_verificacion,
                 $item->razon_social,
+                $item->total_bruto,
+                $item->descuentos,
+                $item->subtotal,
+                $valor_iva,
                 $html_iva,
+                $item->valor_retefuente ? $item->valor_retefuente . "%" : "",
                 $html_rte_fte,
+                $item->valor_reteiva ? $item->valor_reteiva . "%" : "",
                 $html_rte_iva,
+                $item->valor_reteica ? $item->valor_reteica . "%" : "",
                 $html_rte_ica,
                 $item->valor_total,
                 $status,
                 Carbon::parse($item->fecha_elaboracion)->format('d-m-Y'), // Formato de fecha
             ];
         });
+    }
+
+    public function columnFormats(): array
+    {
+        return [
+            /*'D' => NumberFormat::FORMAT_CURRENCY_EUR_SIMPLE,
+            'E' => NumberFormat::FORMAT_CURRENCY_EUR_SIMPLE,
+            'F' => NumberFormat::FORMAT_CURRENCY_EUR_SIMPLE,
+            'H' => NumberFormat::FORMAT_CURRENCY_EUR_SIMPLE,
+            'J' => NumberFormat::FORMAT_CURRENCY_EUR_SIMPLE,
+            'L' => NumberFormat::FORMAT_CURRENCY_EUR_SIMPLE,
+            'N' => NumberFormat::FORMAT_CURRENCY_EUR_SIMPLE,
+            'O' => NumberFormat::FORMAT_CURRENCY_EUR_SIMPLE,*/
+            'Q' => NumberFormat::FORMAT_DATE_DDMMYYYY,
+        ];
+    }
+
+    protected function calculateTotals($data)
+    {
+        $totals = [
+            'Total',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+        ];
+
+        $columnsToSum = [5, 7, 9, 11, 13, 14]; // Índices de las columnas a sumar
+
+        foreach ($columnsToSum as $columnIndex) {
+            $total = collect($data)->sum(function ($item) use ($columnIndex) {
+                $valor = str_replace(',', '.', str_replace('.', '', $item[$columnIndex]));
+                $valor = (float) $valor;
+                return $valor;
+            });
+
+            $totals[$columnIndex] = number_format($total, 2, ',', '.');
+        }
+
+        
+
+        return $totals;
     }
 
     /**
